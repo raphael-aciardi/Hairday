3d99cf75ff2d1bc7f9cfb8f1fb6fea2f20b401a3
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-10-31 09:45:27 -0300

    listing hours

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,5 +1,8 @@
 import dayjs from "dayjs"
 import { openingHours } from "../../utils/opening-hours.js"
+
+const hours = document.getElementById("hours")
+
 export function hoursLoad({date}) {
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":")
@@ -10,6 +13,12 @@ export function hoursLoad({date}) {
       hour,
       avaliable: isHoursPast
     }
-
+  })
+  opening.forEach(({hour, avaliable}) => {
+    const li = document.createElement("li")
+    li.classList.add("hour")
+    li.classList.add(avaliable ? "hour-available" : "hour-unavailable")
+    li.textContent = hour
+    hours.appendChild(li)
   })
 }
